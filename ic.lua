a532e7d53a4ee3d516a6f163ef3593ca2f3a7a02
Ilya Kolbin <iskolbin@gmail.com>
2018-05-07 16:07:16 +0300

    upd

--- ic.lua
+++ ic.lua
@@ -1,5 +1,17 @@
 local ic = {}
 
+function ic.range( x0, dx, n, xs )
+	xs = xs or {}
+	local n_ = #xs
+	for i = 1, n do
+		xs[i] = x0 + dx*(i-1)
+	end
+	for i = n+1, n_ do
+		xs[i] = nil
+	end
+	return xs
+end
+
 function ic.pack1( xs, ys, err, abxs )
 	abxs = abxs or {}
 	local a1, b1 = 0, 0
@@ -27,66 +39,83 @@ function ic.pack1( xs, ys, err, abxs )
 	return abxs
 end
 
-function ic.fpack1( x0, dx, ys, err, abxs )
-	abxs = abxs or {}
-	local a1, b1, x1 = 0, 0, 0
-	local i, j, n, g = 1, 2, #ys, 1
-	if n < 2 then
-		return abxs
-	end
-	local abs = math.abs
-	while j <= n do
-		local a = (ys[i] - ys[j]) / (dx * (i-j))
-		local x = dx * (j-1) + x0
-		local b = ys[j] - a * x
-		local ok = true
-		for u = i+1, j-1 do
-			if abs( a * (dx * (u-1) + x0) + b - ys[u] ) > err then
-				abxs[g], abxs[g+1], abxs[g+2] = a1, b1, x1
-				g, i, ok = g+3, j-1, false
-				break
-			end
-		end
-		if ok then
-			a1, b1, x1, j = a, b, x, j+1
-		end
-	end
-	abxs[g], abxs[g+1], abxs[g+2] = a1, b1, x1
-	return abxs
-end
-
-function ic.funpack1( x0, dx, abxs, xs, ys )
-	xs, ys = xs or {}, ys or {}
-	local x, g, a, b, maxx = x0, 1, 0, 0, 0
+function ic.unpack1( xs, abxs, ys )
+	ys = ys or {}
+	local g, a, b, maxx, x, ok = 1, 0, 0, 0, xs[1], true
 	for i = 1, #abxs, 3 do
 		a, b, maxx = abxs[i], abxs[i+1], abxs[i+2]
-		while x <= maxx do
-			xs[g], ys[g] = x, a*x + b
-			x = x + dx
+		while ok and x <= maxx do
+			ys[g] = a*x + b
 			g = g + 1
+			x = xs[g]
+			ok = x ~= nil
+		end
+		if not ok then
+			break
 		end
 	end
 	if xs[g-1] < maxx then
 		xs[g], ys[g] = maxx, a*maxx + b
 	end
-	return xs, ys
+	return ys
 end
 
 function ic.pack2( xs, ys, err, abcxs )
-	error('uninplemented')
 	abcxs = abcxs or {}
-	local a1, b1, c1 = 0, 0
-	local i, j, n, g = 1, 2, #ys, 1
+	local a1, b1, c1 = 0, 0, 0
+	local l, r, n, g = 1, 3, #ys, 1
 	if n < 2 or #xs ~= n then
 		return abcxs
 	end
 	local abs = math.abs
-	local dx31, dx21, dx32, dy12 = x3-x1, x2-x1, x3-x2, y1-y2
-	local a = (x1*(y2-y3) + x2*(y3-y1) + x3*dy12) / (dx31 * dx32 * dx21)
-	local b = -dy12 / dx21 - (x1+x2)*a
-	local c = y1 - x1*x1*a - x1*b
+	local xl, xr, yl, yr = xs[l], xs[r], ys[l], ys[r]
+	local a, b, c = 0, 0, 0
+	while r <= n do	
+		local ok = true
+		for m = l+1, r-1 do
+			local xm, ym = xs[m], ys[m]
+			local d = (ym - yl) / (xm - xl)
+			a = ((yr - yl) / (xr - xl) - d) / (xr - xm)
+			b = d - (xm + xl)*a
+			c = ym - xl*xl*a - xl*b
+			if abs( a*xm*xm + b*xm + c - ym ) > err then
+				abcxs[g], abcxs[g+1], abcxs[g+2], abcxs[g+3] = a1, b1, c1, xs[r-1]
+				g, l, r, ok = g+4, r-1, r+1, false
+				xl, yl = xs[l], ys[l]
+				if r > n then
+					return abcxs
+				end
+				break
+			end
+		end
+		if ok then
+			a1, b1, c1, r = a, b, c, r+1
+			xr, yr = xs[r], ys[r]
+		end
+	end
+	abcxs[g], abcxs[g+1], abcxs[g+2], abcxs[g+3] = a1, b1, c1, xs[n]
 	return abcxs
 end
 
+function ic.unpack2( xs, abcxs, ys )
+	ys = ys or {}
+	local g, a, b, c, maxx, x, ok = 1, 0, 0, 0, 0, xs[1], true
+	for i = 1, #abcxs, 4 do
+		a, b, c, maxx = abcxs[i], abcxs[i+1], abcxs[i+2], abcxs[i+3]
+		while ok and x <= maxx do
+			ys[g] = a*x*x + b*x + c
+			g = g + 1
+			x = xs[g]
+			ok = x ~= nil
+		end
+		if not ok then
+			break
+		end
+	end
+	if xs[g-1] < maxx then
+		ys[g] = a*maxx*maxx + b*maxx + c
+	end
+	return xs, ys
+end
 
 return ic
--- test/main.lua
+++ test/main.lua
@@ -1,3 +1,4 @@
+local love = assert( _G.love )
 local ic = require('ic')
 
 local xs, ys = {}, {}
@@ -18,20 +19,39 @@ for i = 0, N-1 do
 end
 
 
-local abxs = ic.fpack1( 0, dt, xs, 0.05 )
-local abys = ic.fpack1( 0, dt, ys, 0.05 )
-local ts_, xs_ = ic.funpack1( 0, dt, abxs )
-local ts_, ys_ = ic.funpack1( 0, dt, abys )
-
+local ts = ic.range( 0, dt, #xs )
+local abxs = ic.pack1( ts, xs, 0.05 )
+local abys = ic.pack1( ts, ys, 0.05 )
+local abcys = ic.pack2( ts, ys, 0.05 )
+local xs = ic.unpack1( ts, abxs )
+local ys_ = ic.unpack1( ts, abys )
+local ys2_ = ic.unpack2( ts, abcys )
+--local ts_, xs_ = ic.funpack1( 0, dt, abxs )
+--local ts_, ys_ = ic.funpack1( 0, dt, abys )
+--[[
+local abcys = ic.fpack2( 0, dt, ys, 0.05 )
+local ts2_, ys2_ = ic.funpack2( 0, dt, abcys )
+--]]
 function love.draw()
 	love.graphics.translate( 100, 100 )
 	love.graphics.setPointSize( 4 )
-	love.graphics.setColor( 1, 0, 0 )
+	love.graphics.setColor( 1, 1, 0 )
 	for i = 1, #xs do
 		love.graphics.points( xscl * xs[i], yscl * ys[i] )
 	end
+	love.graphics.setPointSize( 3 )
 	love.graphics.setColor( 0, 1, 0 )
-	for i = 1, #xs_ do
-		love.graphics.points( xscl * xs_[i], yscl * ys_[i] )
+	for i = 1, #xs do
+		love.graphics.points( xscl * xs[i], yscl * ys_[i] )
 	end
+	love.graphics.setPointSize( 2 )
+	love.graphics.setColor( 1, 0, 0 )
+	for i = 1, #xs do
+		love.graphics.points( xscl * xs[i], yscl * ys2_[i] )
+	end
+	--[[
+	for i = 1, #ys2_ do
+		love.graphics.points( xscl * xs_[i], yscl * ys2_[i] + 400 )
+	end
+	--]]
 end
